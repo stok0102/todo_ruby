33f7366cbe28b4fab44122b53c102b7fac8d1cea
Chip Carnes and Caleb Stokka <chip@>
2016-07-11 13:39:24 -0700

    Add first integration spec

--- app.rb
+++ app.rb
@@ -2,18 +2,29 @@ require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/task")
+require("./lib/list")
 require('pg')
 
-DB = PG.connect({:dbname => 'to_do'})
+# DB = PG.connect({:dbname => 'to_do'})
 
 get("/") do
-  @tasks = Task.all()
   erb(:index)
 end
 
-post("/tasks") do
-  description = params.fetch("description")
-  task = Task.new(description)
-  task.save()
-  erb(:success)
+get('/lists/new') do
+  erb(:list_form)
 end
+
+post('/lists') do
+  name = params.fetch('name')
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:list_success)
+end
+
+# post("/tasks") do
+#   description = params.fetch("description")
+#   task = Task.new(description)
+#   task.save()
+#   erb(:success)
+# end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -3,8 +3,15 @@ require("spec_helper")
 describe(Task) do
   describe("#description") do
     it("lets you give it a description") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2016-07-12'})
       expect(test_task.description()).to(eq("learn SQL"))
+    end
+  end
+
+  describe("#due_date") do
+    it("lets you give it a due date") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2016-07-12'})
+      expect(test_task.due_date()).to(eq('2016-07-12'))
     end
   end
 
@@ -16,7 +23,7 @@ describe(Task) do
 
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2016-07-12'})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
@@ -24,15 +31,15 @@ describe(Task) do
 
   describe("#list_id") do
     it "lets you read the list ID out" do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2016-07-12'})
       expect(test_task.list_id()).to(eq(1))
     end
   end
 
   describe("#==") do
     it("is the same task if it has the same description") do
-      task1 = Task.new({:description => "learn SQL", :list_id => 1})
-      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      task1 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2016-07-12'})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2016-07-12'})
       expect(task1).to(eq(task2))
     end
   end
